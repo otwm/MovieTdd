1028de0e96706c3592636176fccc7c023fce14d3
kdo <otwmoo@gmail.com>
2016-02-06 17:56:00 +0900

    inspection

--- src/main/java/movie/Movie.java
+++ src/main/java/movie/Movie.java
@@ -1,14 +1,21 @@
 package movie;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * 무비
  * Created by kdo on 16. 2. 6.
  */
-public class Movie {
-    public int averateRating() {
-        return 0;
+class Movie {
+    private final List<Integer> someOfRates = new ArrayList<>();
+
+    public int averageRate() {
+        if (someOfRates.size() == 0) return 0;
+        return someOfRates.stream().mapToInt(rate -> rate).sum() / someOfRates.size();
     }
 
-    public void rate(int i) {
-
+    public void rate(int rate) {
+        this.someOfRates.add(rate);
     }
 }
--- src/test/java/movie/MovieTest.java
+++ src/test/java/movie/MovieTest.java
@@ -1,29 +1,39 @@
 package movie;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
+ * 무비 테스트
  * Created by kdo on 16. 2. 6.
  */
 public class MovieTest {
-    @Test
-    public void foo(){
 
+    private Movie movie;
+
+    @Before
+    public void setUp() throws Exception {
+        movie = new Movie();
     }
 
     @Test
-    public void should_return_0_movie_just_created(){
-        Movie movie = new Movie();
-        assertThat(movie.averateRating(),is(0));
+    public void 무비가_처음_만들어졌을때는_0이_나와야함(){
+        assertThat(movie.averageRate(),is(0));
     }
 
     @Test
-    public void should_return_1_when_1_was_rated(){
-        Movie movie = new Movie();
+    public void 무비에_레이팅을_1_설정하면_평균은_1이_나와야한다(){
         movie.rate(1);
-        assertThat(movie.averateRating(),is(0));
+        assertThat(movie.averageRate(),is(1));
+    }
+
+    @Test
+    public void 무비에_레이팅2와4를_설정하면_3이나와야_한다(){
+        movie.rate(2);
+        movie.rate(4);
+        assertThat(movie.averageRate(),is(3));
     }
 }
